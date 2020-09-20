950647bd0cd1a8d1c457f9a063e3ecdfd5b57f2d
Sanju S <spikeysanju98@gmail.com>
2020-09-20 12:28:48 +0530

    Add comments
    Signed-off-by: Sanju S <spikeysanju98@gmail.com>

--- app/src/main/java/www/spikeysanju/jetquotes/utils/CTAUtils.kt
+++ app/src/main/java/www/spikeysanju/jetquotes/utils/CTAUtils.kt
@@ -33,13 +33,14 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 
-
+// used to copy quote to clipboard
 fun Context.copyToClipboard(text: CharSequence) {
     val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
     val clip = ClipData.newPlainText("label", text)
     clipboard.setPrimaryClip(clip)
 }
 
+// used to share quote to other application
 fun Context.shareToOthers(quote: String) {
     val intent = Intent(Intent.ACTION_SEND)
     intent.type = "text/plain"
