ba749bd29427a7c5b329165c4b95b2a809c06d9b
Sanju S <spikeysanju98@gmail.com>
2021-05-04 09:44:19 +0530

    Set Schema Export #24
    Signed-off-by: Spikeysanju <spikeysanju98@gmail.com>

--- app/src/main/java/www/spikeysanju/jetquotes/data/preference/db/JetQuotesDatabase.kt
+++ app/src/main/java/www/spikeysanju/jetquotes/data/preference/db/JetQuotesDatabase.kt
@@ -32,7 +32,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import www.spikeysanju.jetquotes.model.Quote
 
-@Database(entities = [Quote::class], version = 1)
+@Database(entities = [Quote::class], version = 1, exportSchema = false)
 abstract class JetQuotesDatabase : RoomDatabase() {
     abstract fun getFavouritesDao(): FavouritesDao
 }
