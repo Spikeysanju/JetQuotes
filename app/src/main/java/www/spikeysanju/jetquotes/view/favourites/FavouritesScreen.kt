9c8f69b1d13ca3d7b2c2ee68c5cf12be05aafd87
Sanju S <spikeysanju98@gmail.com>
2021-05-04 09:37:32 +0530

    Add Swipe to Delete #24
    Signed-off-by: Spikeysanju <spikeysanju98@gmail.com>

--- app/src/main/java/www/spikeysanju/jetquotes/view/favourites/FavouritesScreen.kt
+++ app/src/main/java/www/spikeysanju/jetquotes/view/favourites/FavouritesScreen.kt
@@ -28,24 +28,52 @@
 
 package www.spikeysanju.jetquotes.view.favourites
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.Icon
+import androidx.compose.material.ListItem
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import www.spikeysanju.jetquotes.components.QuotesCard
 import www.spikeysanju.jetquotes.components.TopBarWithBack
 import www.spikeysanju.jetquotes.navigation.MainActions
+import www.spikeysanju.jetquotes.ui.red
 import www.spikeysanju.jetquotes.utils.FavouriteViewState
 import www.spikeysanju.jetquotes.view.viewModel.MainViewModel
 
+@ExperimentalMaterialApi
 @Composable
 fun FavouritesScreen(viewModel: MainViewModel, actions: MainActions) {
     Scaffold(topBar = {
-
         TopBarWithBack(
-            title = "JetQuotes",
+            title = "Favourites",
             onBackClick = { actions.upPress() })
 
     }, content = {
@@ -57,7 +85,73 @@ fun FavouritesScreen(viewModel: MainViewModel, actions: MainActions) {
             is FavouriteViewState.Success -> {
                 LazyColumn {
                     items(result.quote) { quote ->
-                        QuotesCard(quote = quote, actions = actions)
+                        var unread by remember { mutableStateOf(false) }
+                        val dismissState = rememberDismissState(
+                            confirmStateChange = {
+                                if (it == DismissValue.DismissedToEnd) unread = !unread
+                                it != DismissValue.DismissedToEnd
+                            }
+                        )
+
+                        SwipeToDismiss(
+                            state = dismissState,
+                            modifier = Modifier.padding(vertical = 4.dp),
+                            directions = setOf(
+                                DismissDirection.StartToEnd,
+                                DismissDirection.EndToStart
+                            ),
+                            dismissThresholds = { direction ->
+                                FractionalThreshold(if (direction == DismissDirection.StartToEnd) 0.25f else 0.5f)
+                            },
+                            background = {
+                                val direction =
+                                    dismissState.dismissDirection ?: return@SwipeToDismiss
+                                val color by animateColorAsState(
+                                    when (dismissState.targetValue) {
+                                        DismissValue.Default -> MaterialTheme.colors.background
+                                        DismissValue.DismissedToEnd -> red
+                                        DismissValue.DismissedToStart -> red
+                                    }
+                                )
+                                val alignment = when (direction) {
+                                    DismissDirection.StartToEnd -> Alignment.CenterStart
+                                    DismissDirection.EndToStart -> Alignment.CenterEnd
+                                }
+                                val icon = when (direction) {
+                                    DismissDirection.StartToEnd -> Icons.Outlined.Delete
+                                    DismissDirection.EndToStart -> Icons.Outlined.Delete
+                                }
+                                val scale by animateFloatAsState(
+                                    if (dismissState.targetValue == DismissValue.Default) 0.75f else 1.5f
+                                )
+
+                                Box(
+                                    Modifier
+                                        .fillMaxSize()
+                                        .background(color)
+                                        .padding(horizontal = 20.dp),
+                                    contentAlignment = alignment
+                                ) {
+                                    Icon(
+                                        icon,
+                                        contentDescription = "Localized description",
+                                        modifier = Modifier
+                                            .scale(scale)
+                                            .clickable { viewModel.deleteFavourite(quote) },
+                                        tint = Color.White
+                                    )
+                                }
+                            },
+                            dismissContent = {
+                                ListItem(
+                                    text = {
+                                        QuotesCard(quote = quote, actions = actions)
+                                    },
+                                    secondaryText = { Text("Swipe to Remove from favourites!") }
+                                )
+                            }
+                        )
+
                     }
                 }
             }
